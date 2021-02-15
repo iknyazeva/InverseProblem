5834e652af1ff5e4f37491969b027766508fa555
myxa <33979138+myxa@users.noreply.github.com>
2021-02-15 18:47:14 +0300

    обновила requirements

---
+++ tests/test_main_1d.py
@@ -0,0 +1,58 @@
+import pytest
+import torch
+
+from inverse_problem.nn_inversion.conv1dmodel import Conv1dModel
+from inverse_problem.nn_inversion.main import HyperParams, Model
+
+
+class TestMain:
+
+    @pytest.fixture
+    def sample_batch(self):
+        hps = HyperParams()
+        model = Model(hps)
+        train_loader = model.make_loader()
+        it = iter(train_loader)
+        sample_batch = next(it)
+        return sample_batch
+
+    def test_model_init(self):
+        hps = HyperParams()
+        model = Model(hps)
+        model.net = Conv1dModel(hps)
+        assert True
+
+    def test_model_make_loader(self):
+        hps = HyperParams()
+        model = Model(hps)
+        model.net = Conv1dModel(hps)
+        train_loader = model.make_loader()
+        it = iter(train_loader)
+        sample_batch = next(it)
+        assert sample_batch['X'].size() == (20, 225)
+        assert sample_batch['Y'].size() == (20, 11)
+
+        assert isinstance(sample_batch['X'][0], torch.Tensor)
+        assert isinstance(sample_batch['Y'][0], torch.Tensor)
+
+    def test_model_fit_step(self, sample_batch):
+        hps = HyperParams()
+        model = Model(hps)
+        model.net = Conv1dModel(hps)
+        loss = model.fit_step(sample_batch)
+        assert True
+
+    def test_model_eval_step(self, sample_batch):
+        hps = HyperParams()
+        model = Model(hps)
+        model.net = Conv1dModel(hps)
+        loss = model.eval_step(sample_batch)
+        assert True
+
+    def test_model_train(self):
+        hps = HyperParams()
+        hps.per_epoch = 2
+        model = Model(hps)
+        model.net = Conv1dModel(hps)
+        history = model.train()
+        assert True
